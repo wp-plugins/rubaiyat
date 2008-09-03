0f4f02313531941bd4468367fb12a6c7af212ceb
pleft <pleft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-03 03:59:52 +0000

    First version added
    git-svn-id: https://plugins.svn.wordpress.org/rubaiyat/trunk@62481 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rubaiyat.php
@@ -0,0 +1,121 @@
+<?php
+/*
+Plugin Name: Rubaiyat
+Plugin URI: http://www.verso.co.nz/resources
+Description: based on Matt Mullenweg's 'Hello Dolly' plugin. 
+Author: Paul Left
+Version: 0.1
+Author URI: http://www.verso.co.nz/about/
+*/
+
+// These are the first 30 stanzas of The Rubaiyat
+$lyrics = "Awake! for Morning in the Bowl of Night<br />Has flung the Stone that puts the Stars to Flight:<br /> And Lo! the Hunter of the East has caught<br />The Sultan's Turret in a Noose of Light. (I)
+Dreaming when Dawn's Left Hand was in the Sky<br />I heard a Voice within the Tavern cry,<br /> &quot;Awake, my Little ones, and fill the Cup<br />Before Life's Liquor in its Cup be dry.&quot; (II)
+And, as the Cock crew, those who stood before<br />The Tavern shouted--&quot;Open then the Door.<br /> You know how little while we have to stay,<br />And, once departed, may return no more.&quot; (III)
+Now the New Year reviving old Desires,<br />The thoughtful Soul to Solitude retires,<br /> Where the WHITE HAND OF MOSES on the Bough<br />Puts out, and Jesus from the Ground suspires. (IV)
+Iram indeed is gone with all its Rose,<br />And Jamshyd's Sev'n-ring'd Cup where no one knows;<br /> But still the Vine her ancient Ruby yields,<br />And still a Garden by the Water blows. (V)
+And David's Lips are lock't; but in divine<br />High piping Pelevi, with &quot;Wine!  Wine!  Wine!<br /> Red Wine!&quot;--the Nightingale cries to the Rose<br />That yellow Cheek of hers to'incarnadine. (VI)
+Come, fill the Cup, and in the Fire of Spring<br />The Winter Garment of Repentance fling:<br /> The Bird of Time has but a little way<br />To fly--and Lo! the Bird is on the Wing. (VII)
+And look--a thousand Blossoms with the Day<br />Woke--and a thousand scatter'd into Clay:<br /> And this first Summer Month that brings the Rose<br />Shall take Jamshyd and Kaikobad away. (VIII)
+But come with old Khayyam, and leave the Lot<br />Of Kaikobad and Kaikhosru forgot:<br /> Let Rustum lay about him as he will,<br />Or Hatim Tai cry Supper--heed them not. (IX)
+With me along some Strip of Herbage strown<br />That just divides the desert from the sown,<br /> Where name of Slave and Sultan scarce is known,<br />And pity Sultan Mahmud on his Throne. (X)
+Here with a Loaf of Bread beneath the Bough,<br />A Flask of Wine, a Book of Verse--and Thou<br /> Beside me singing in the Wilderness--<br />And Wilderness is Paradise enow. (XI)
+&quot;How sweet is mortal Sovranty!&quot;--think some:<br />Others--&quot;How blest the Paradise to come!&quot;<br /> Ah, take the Cash in hand and waive the Rest;<br />Oh, the brave Music of a distant Drum! (XII)
+Look to the Rose that blows about us--&quot;Lo,<br />Laughing,&quot; she says, &quot;into the World I blow:<br /> At once the silken Tassel of my Purse<br />Tear, and its Treasure on the Garden throw.&quot; (XIII)
+The Worldly Hope men set their Hearts upon<br />Turns Ashes--or it prospers; and anon,<br /> Like Snow upon the Desert's dusty Face<br />Lighting a little Hour or two--is gone. (XIV)
+And those who husbanded the Golden Grain,<br />And those who flung it to the Winds like Rain,<br /> Alike to no such aureate Earth are turn'd<br />As, buried once, Men want dug up again. (XV)
+Think, in this batter'd Caravanserai<br />Whose Doorways are alternate Night and Day,<br /> How Sultan after Sultan with his Pomp<br />Abode his Hour or two, and went his way. (XVI)
+They say the Lion and the Lizard keep<br />The Courts where Jamshyd gloried and drank deep:<br /> And Bahram, that great Hunter--the Wild Ass<br />Stamps o'er his Head, and he lies fast asleep. (XVII)
+ I sometimes think that never blows so red<br />The Rose as where some buried Caesar bled;<br /> That every Hyacinth the Garden wears<br />Dropt in its Lap from some once lovely Head. (XVIII)
+And this delightful Herb whose tender Green<br />Fledges the River's Lip on which we lean--<br /> Ah, lean upon it lightly! for who knows<br />From what once lovely Lip it springs unseen! (XIX)
+Ah! my Beloved, fill the Cup that clears<br />TO-DAY of past Regrets and future Fears-<br /> To-morrow?--Why, To-morrow I may be<br />Myself with Yesterday's Sev'n Thousand Years. (XX)
+Lo! some we loved, the loveliest and the best<br />That Time and Fate of all their Vintage prest,<br /> Have drunk their Cup a Round or two before,<br />And one by one crept silently to Rest. (XXI)
+And we, that now make merry in the Room<br />They left, and Summer dresses in new Bloom,<br /> Ourselves must we beneath the Couch of Earth<br />Descend, ourselves to make a Couch--for whom? (XXII)
+Ah, make the most of what we yet may spend,<br />Before we too into the Dust Descend;<br /> Dust into Dust, and under Dust, to lie,<br />Sans Wine, sans Song, sans Singer and--sans End! (XXIII)
+Alike for those who for TO-DAY prepare,<br />And those that after a TO-MORROW stare,<br /> A Muezzin from the Tower of Darkness cries<br />&quot;Fools! your Reward is neither Here nor There.&quot; (XXIV)
+Why, all the Saints and Sages who discuss'd<br />Of the Two Worlds so learnedly, are thrust<br /> Like foolish Prophets forth; their Words to Scorn<br />Are scatter'd, and their Mouths are stopt with Dust. (XXV)
+Oh, come with old Khayyam, and leave the Wise<br />To talk; one thing is certain, that Life flies;<br /> One thing is certain, and the Rest is Lies;<br />The Flower that once has blown for ever dies. (XXVI)
+Myself when young did eagerly frequent<br />Doctor and Saint, and heard great Argument<br /> About it and about: but evermore<br />Came out by the same Door as in I went. (XXVII)
+With them the Seed of Wisdom did I sow,<br />And with my own hand labour'd it to grow:<br /> And this was all the Harvest that I reap'd--<br />&quot;I came like Water, and like Wind I go.&quot; (XXVIII)
+ Into this Universe, and why not knowing,<br />Nor whence, like Water willy-nilly flowing:<br /> And out of it, as Wind along the Waste,<br />I know not whither, willy-nilly blowing. (XXIX)
+What, without asking, hither hurried whence?<br />And, without asking, whither hurried hence!<br /> Another and another Cup to drown<br />The Memory of this Impertinence! (XXX)
+Up from Earth's Centre through the seventh Gate<br />I rose, and on the Throne of Saturn sate,<br /> And many Knots unravel'd by the Road;<br />But not the Knot of Human Death and Fate. (XXXI)
+There was a Door to which I found no Key:<br />There was a Veil past which I could not see:<br /> Some little Talk awhile of ME and THEE<br />There seemed--and then no more of THEE and ME. (XXXII)
+Then to the rolling Heav'n itself I cried,<br />Asking, &quot;What Lamp had Destiny to guide<br /> Her little Children stumbling in the Dark?&quot;<br />And--&quot;A blind understanding!&quot; Heav'n replied. (XXXIII)
+Then to this earthen Bowl did I adjourn<br />My Lip the secret Well of Life to learn:<br /> And Lip to Lip it murmur'd--&quot;While you live,<br />Drink!--for once dead you never shall return.&quot; (XXXIV)
+I think the Vessel, that with fugitive<br />Articulation answer'd, once did live,<br /> And merry-make; and the cold Lip I kiss'd<br />How many Kisses might it take--and give. (XXXV)
+For in the Market-place, one Dusk of Day,<br />I watch'd the Potter thumping his wet Clay:<br /> And with its all obliterated Tongue<br />It murmur'd--&quot;Gently, Brother, gently, pray!&quot; (XXXVI)
+Ah, fill the Cup:--what boots it to repeat<br />How Time is slipping underneath our Feet:<br /> Unborn TO-MORROW and dead YESTERDAY,<br />Why fret about them if TO-DAY be sweet! (XXXVII)
+One Moment in Annihilation's Waste,<br />One moment, of the Well of Life to taste--<br /> The Stars are setting, and the Caravan<br />Starts for the dawn of Nothing--Oh, make haste! (XXXVIII)
+How long, how long, in infinite Pursuit<br />Of This and That endeavour and dispute?<br /> Better be merry with the fruitful Grape<br />Than sadden after none, or bitter, Fruit. (XXXIX)
+You know, my Friends, how long since in my House<br />For a new Marriage I did make Carouse:<br /> Divorced old barren Reason from my Bed,<br />And took the Daughter of the Vine to Spouse. (XL)
+For &quot;IS&quot; and &quot;IS-NOT&quot; though with Rule and Line,<br />And, &quot;UP-AND-DOWN&quot; without, I could define,<br /> I yet in all I only cared to know,<br />Was never deep in anything but--Wine. (XLI)
+And lately, by the Tavern Door agape,<br />Came stealing through the Dusk an Angel Shape,<br /> Bearing a vessel on his Shoulder; and<br />He bid me taste of it; and 'twas--the Grape! (XLII)
+The Grape that can with Logic absolute<br />The Two-and-Seventy jarring Sects confute:<br /> The subtle Alchemist that in a Trice<br />Life's leaden Metal into Gold transmute. (XLIII)
+The mighty Mahmud, the victorious Lord,<br />That all the misbelieving and black Horde<br /> Of Fears and Sorrows that infest the Soul<br />Scatters and slays with his enchanted Sword. (XLIV)
+But leave the Wise to wrangle, and with me<br />The Quarrel of the Universe let be:<br /> And, in some corner of the Hubbub coucht,<br />Make Game of that which makes as much of Thee. (XLV)
+For in and out, above, about, below,<br />'Tis nothing but a Magic Shadow-show,<br /> Play'd in a Box whose Candle is the Sun,<br />Round which we Phantom Figures come and go. (XLVI)
+And if the Wine you drink, the Lip you press,<br />End in the Nothing all Things end in--Yes-<br /> Then fancy while Thou art, Thou art but what<br />Thou shalt be--Nothing--Thou shalt not be less. (XLVII)
+While the Rose blows along the River Brink,<br />With old Khayyam the Ruby Vintage drink:<br /> And when the Angel with his darker Draught<br />Draws up to thee--take that, and do not shrink. (XLVIII)
+Tis all a Chequer-board of Nights and Days<br />Where Destiny with Men for Pieces plays:<br /> Hither and thither moves, and mates, and slays,<br />And one by one back in the Closet lays. (XLIX)
+The Ball no Question makes of Ayes and Noes,<br />But Right or Left as strikes the Player goes;<br /> And He that toss'd Thee down into the Field,<br />He knows about it all--HE knows--HE knows! (L)
+The Moving Finger writes; and, having writ,<br />Moves on: nor all thy Piety nor Wit<br /> Shall lure it back to cancel half a Line,<br />Nor all thy Tears wash out a Word of it. (LI)
+And that inverted Bowl we call The Sky,<br />Whereunder crawling coop't we live and die,<br /> Lift not thy hands to IT for help--for It<br />Rolls impotently on as Thou or I. (LII)
+With Earth's first Clay They did the Last Man's knead,<br />And then of the Last Harvest sow'd the Seed:<br /> Yea, the first Morning of Creation wrote<br />What the Last Dawn of Reckoning shall read. (LIII)
+I tell Thee this--When, starting from the Goal,<br />Over the shoulders of the flaming Foal<br /> Of Heav'n Parwin and Mushtari they flung,<br />In my predestin'd Plot of Dust and Soul (LIV)
+The Vine had struck a Fibre; which about<br />It clings my Being--let the Sufi flout;<br /> Of my Base Metal may be filed a Key,<br />That shall unlock the Door he howls without. (LV)
+And this I know: whether the one True Light,<br />Kindle to Love, or Wrath consume me quite,<br /> One Glimpse of It within the Tavern caught<br />Better than in the Temple lost outright. (LVI)
+Oh Thou who didst with Pitfall and with Gin<br />Beset the Road I was to wander in,<br /> Thou wilt not with Predestination round<br />Enmesh me, and impute my Fall to Sin? (LVII)
+Oh Thou, who Man of baser Earth didst make,<br />And who with Eden didst devise the Snake;<br /> For all the Sin wherewith the Face of Man<br />Is blacken'd, Man's Forgiveness give--and take! (LVIII)
+Listen again.  One Evening at the Close<br />Of Ramazan, ere the better Moon arose,<br /> In that old Potter's Shop I stood alone<br />With the clay Population round in Rows. (LIX)
+And strange to tell, among that Earthen Lot<br />Some could articulate, while others not:<br /> And suddenly one more impatient cried--<br />&quot;Who is the Potter, pray, and who the Pot?&quot; (LX)
+Then said another--&quot;Surely not in vain<br />My substance from the common Earth was ta'en,<br /> That He who subtly wrought me into Shape<br />Should stamp me back to common Earth again.&quot; (LXI)
+Another said--&quot;Why, ne'er a peevish Boy<br />Would break the Bowl from which he drank in Joy;<br /> Shall He that made the Vessel in pure Love<br />And Fansy, in an after Rage destroy!&quot; (LXII)
+None answer'd this; but after Silence spake<br />A Vessel of a more ungainly Make:<br /> &quot;They sneer at me for leaning all awry;<br />What? did the Hand then of the Potter shake?&quot; (LXIII)
+Said one--&quot;Folks of a surly Tapster tell,<br />And daub his Visage with the Smoke of Hell;<br /> They talk of some strict Testing of us--Pish!<br />He's a Good Fellow, and 'twill all be well.&quot; (LXIV)
+Then said another with a long-drawn Sigh,<br />&quot;My Clay with long oblivion is gone dry:<br /> But, fill me with the old familiar Juice,<br />Methinks I might recover by-and-bye!&quot; (LXV)
+So, while the Vessels one by one were speaking,<br />One spied the little Crescent all were seeking:<br /> And then they jogg'd each other, &quot;Brother! Brother!<br />Hark to the Porter's Shoulder-knot a-creaking!&quot; (LXVI)
+Ah, with the Grape my fading Life provide,<br />And wash my Body whence the life has died,<br /> And in a Windingsheet of Vineleaf wrapt,<br />So bury me by some sweet Gardenside. (LXVII)
+That ev'n my buried Ashes such a Snare<br />Of Perfume shall fling up into the Air,<br /> As not a True Believer passing by<br />But shall be overtaken unaware. (LXVIII)
+Indeed, the Idols I have loved so long<br />Have done my Credit in Men's Eye much wrong:<br /> Have drown'd my Honour in a shallow Cup,<br />And sold my Reputation for a Song. (LXIX)
+Indeed, indeed, Repentance oft before<br />I swore--but was I sober when I swore?<br /> And then and then came Spring, and Rose-in-hand<br />My thread-bare Penitence a-pieces tore. (LXX)
+And much as Wine has play'd the Infidel,<br />And robb'd me of my Robe of Honour--well,<br /> I often wonder what the Vintners buy<br />One half so precious as the Goods they sell. (LXXI)
+Alas, that Spring should vanish with the Rose!<br />That Youth's sweet-scented Manuscript should close!<br /> The Nightingale that in the Branches sang,<br />Ah, whence, and whither flown again, who knows! (LXII)
+Ah, Love! could thou and I with Fate conspire<br />To grasp this sorry Scheme of Things entire,<br /> Would not we shatter it to bits--and then<br />Re-mould it nearer to the Heart's Desire! (LXIII)
+Ah, Moon of my Delight who know'st no wane,<br />The Moon of Heav'n is rising once again:<br /> How oft hereafter rising shall she look<br />Through this same Garden after me--in vain! (LXXIV)
+And when Thyself with shining Foot shall pass<br />Among the Guests Star-scatter'd on The Grass,<br /> And in Thy joyous Errand reach the Spot<br />Where I made one--turn down an empty Glass! (LXXV)";
+
+// Here we split it into lines
+$lyrics = explode("\n", $lyrics);
+// And then randomly choose a line
+$chosen = wptexturize( $lyrics[ mt_rand(0, count($lyrics) - 1) ] );
+
+// This just echoes the chosen line, we'll position it later
+function show_lyrics() {
+	global $chosen;
+	echo "<p id='extract'>$chosen</p>";
+}
+
+// Now we set that function up to execute when the admin_footer action is called
+add_action('admin_footer', 'show_lyrics');
+
+// We need some CSS to position the paragraph
+function extract_css() {
+	echo "
+	<style type='text/css'>
+	#extract {
+		position: absolute;
+		top: 3.6em;
+		margin: 0;
+		padding: 0;
+		right: 10px;
+		font-size: 10px;
+		color: #d5c5c5;
+	}
+	</style>
+	";
+}
+
+add_action('admin_head', 'extract_css');
+
+?>
